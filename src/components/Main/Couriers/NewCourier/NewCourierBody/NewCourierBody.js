8af14446361f63364dbeb5989deb007e6383c01a
Ralph Lauren <rl@example.com>
2022-12-19 17:02:03 +0200

    feat: dirty  new courier7

--- src/components/Main/Couriers/NewCourier/NewCourierBody/NewCourierBody.js
+++ src/components/Main/Couriers/NewCourier/NewCourierBody/NewCourierBody.js
@@ -16,12 +16,13 @@ const NewCourierBody = () => {
   });
 
   const onSubmit = (data) => {
-    alert(JSON.stringify(data));
+    // alert(JSON.stringify(data));
+    console.log(data)
     reset()
   }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form className="new-courier-form" onSubmit={handleSubmit(onSubmit)}>
       <div className="new-courier-body">
         <div className="new-courier-1clm new-courier-clm">
 
@@ -29,6 +30,38 @@ const NewCourierBody = () => {
             <div className="new-courier-line__description">Name</div>
             <div>
               <input {...register('firstName', {
+                required: 'Name is required',
+                minLength: {
+                  value: 5,
+                  message: 'Minimum 5 letters'
+                }
+              })} />
+            </div>
+            <div className="new-courier-line__error">
+              {errors?.firstName && <p>{errors?.firstName?.message || "Error!"}</p>}
+            </div>
+          </label>
+
+          <label className="new-courier-line">
+            <div className="new-courier-line__description">Holder Name</div>
+            <div>
+              <input {...register('holderName', {
+                required: 'Holder Name is required',
+                minLength: {
+                  value: 5,
+                  message: 'Minimum 5 letters'
+                }
+              })} />
+            </div>
+            <div className="new-courier-line__error">
+              {errors?.firstName && <p>{errors?.holderName?.message || "Error!"}</p>}
+            </div>
+          </label>
+
+          <label className="new-courier-line">
+            <div className="new-courier-line__description">DOB (mm/dd/yyyy)</div>
+            <div>
+              <input {...register('DOB', {
                 required: 'Field is required',
                 minLength: {
                   value: 5,
@@ -41,11 +74,6 @@ const NewCourierBody = () => {
             </div>
           </label>
           
-          
-          <div className="new-courier-line__description">Holder Name</div>
-          <div></div>
-          <div className="new-courier-line__description">DOB (mm/dd/yyyy)</div>
-          <div></div>
           <div className="new-courier-line__description">Cell Phone</div>
           <div></div>
           <div className="new-courier-line__description">Street Address</div>
@@ -84,6 +112,7 @@ const NewCourierBody = () => {
         <div className="full-info-4clm full-info-clm">
           place for chat
           <input type="submit" disabled={!isValid}/>
+          <input type="button" onClick={()=>{console.log('reset'); document.querySelector(".new-courier-form").reset()}} value="reset"/>
         </div>
       </div>
     </form>
